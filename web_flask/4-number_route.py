bfe90644420837c036b79685812647857a9b97a3
Eidal559 <eidaljuarbe@gmail.com>
2024-04-08 12:22:27 -0400

    updated tasks

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,37 +1,44 @@
 #!/usr/bin/python3
-"""Importing the needed framework"""
+'''A simple Flask web application.
+'''
 from flask import Flask
 
+
 app = Flask(__name__)
+'''The Flask application instance.'''
+app.url_map.strict_slashes = False
 
-@app.route("/", strict_slashes=False)
-def hello():
-    """Function that returns a simple string"""
-    return "Hello HBNB!"
 
-@app.route("/hbnb", strict_slashes=False)
-def hello_hbnb():
-    """Function that returns HBNB string"""
-    return "HBNB"
+@app.route('/')
+def index():
+    '''The home page.'''
+    return 'Hello HBNB!'
 
-@app.route("/c/<text>", strict_slashes=False)
-def c_print(text):
-    """Function that returns a C string"""
-    result = text.replace('_', ' ')
-    return "C {}".format(result)
 
-@app.route("/python", strict_slashes=False)
-@app.route("/python/<text>", strict_slashes=False)
-def python_print(text="is cool"):
-    """Function that returns a Python string"""
-    result = text.replace('_', ' ')
-    return "Python {}".format(result)
+@app.route('/hbnb')
+def hbnb():
+    '''The hbnb page.'''
+    return 'HBNB'
 
-@app.route("/number/<int:n>", strict_slashes=False)
-def num(n):
-    """Function that returns a string only if n is a number"""
-    if type(n) == int:
-        return "{} is a number".format(n)
+
+@app.route('/c/<text>')
+def c_page(text):
+    '''The c page.'''
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+@app.route('/python/<text>')
+@app.route('/python', defaults={'text': 'is cool'})
+def python_page(text):
+    '''The python page.'''
+    return 'Python {}'.format(text.replace('_', ' '))
+
+
+@app.route('/number/<int:n>')
+def number_page(n):
+    '''The number page.'''
+    return '{} is a number'.format(n)
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port='5000')
