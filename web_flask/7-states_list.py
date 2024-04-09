6630a98242b84ad2c0165593976345fc42dfee51
Eidal559 <eidaljuarbe@gmail.com>
2024-04-09 12:40:58 -0400

    updated tasks

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,33 +1,27 @@
 #!/usr/bin/python3
-'''A simple Flask web application.
-'''
-from flask import Flask, render_template
+"""Script that starts a Flask web application."""
 
+from flask import Flask, render_template
 from models import storage
 from models.state import State
 
-
 app = Flask(__name__)
-'''The Flask application instance.'''
 app.url_map.strict_slashes = False
 
 
 @app.route('/states_list')
 def states_list():
-    '''The states_list page.'''
-    all_states = list(storage.all(State).values())
-    all_states.sort(key=lambda x: x.name)
-    ctxt = {
-        'states': all_states
-    }
-    return render_template('7-states_list.html', **ctxt)
+    """Display a HTML page with a list of all State objects."""
+    states = storage.all(State).values()
+    sorted_states = sorted(states, key=lambda state: state.name)
+    return render_template('states_list.html', states=sorted_states)
 
 
 @app.teardown_appcontext
-def flask_teardown(exc):
-    '''The Flask app/request context end event listener.'''
+def teardown_db(exception):
+    """Close the current SQLAlchemy Session."""
     storage.close()
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port=5000)
