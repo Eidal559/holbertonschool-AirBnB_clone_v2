efb1d1ecd1b4761813564e14d743177eb285a514
Eidal559 <eidaljuarbe@gmail.com>
2024-04-09 13:23:17 -0400

    updated task 11

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -1,28 +1,31 @@
 #!/usr/bin/python3
-"""Importing Flask to run the web app"""
-from flask import Flask, render_template
-from models import storage
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
+"""Starts the Flask web application.
 
+The application listens 0.0.0.0, port 5000.
+Routes:
+    /hbnb_filters: HBnB HTML filters page.
+"""
+from models import storage
+from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 
+@app.route("/hbnb_filters", strict_slashes=False)
+def hbnb_filters():
+    """Displays the main HBnB filters HTML page."""
+    states = storage.all("State")
+    amenities = storage.all("Amenity")
+    return render_template("10-hbnb_filters.html",
+                           states=states, amenities=amenities)
+
+
 @app.teardown_appcontext
-def close(self):
-    """ Method to close the session """
+def teardown(exc):
+    """Remove the current SQLAlchemy session."""
     storage.close()
 
 
-@app.route('/hbnb_filters', strict_slashes=False)
-def hbnb_filters():
-    """Displays a html page with from web static"""
-    states = storage.all(State).values()
-    amenities = storage.all(Amenity).values()
-    return render_template('10-hbnb_filters.html', states=states, amenities=amenities)
-
-
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port="5000")
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
