1d27e411b02916b04096fcdf97365c8d8366be6e
Eidal559 <eidaljuarbe@gmail.com>
2024-04-14 21:55:59 -0400

    updated task 11

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -1,27 +1,33 @@
 #!/usr/bin/python3
-"""
-starts a Flask web application
-"""
-
+""" Script that starts a Flask web application """
 from flask import Flask, render_template
-from models import *
 from models import storage
+from models.state import State
+from models.amenity import Amenity
+
+
 app = Flask(__name__)
 
 
-@app.route('/hbnb_filters', strict_slashes=False)
-def filters():
-    """display a HTML page like 6-index.html from static"""
-    states = storage.all("State").values()
-    amenities = storage.all("Amenity").values()
-    return render_template('10-hbnb_filters.html', states=states,
-                           amenities=amenities)
+@app.route('/hbnb_filters/', strict_slashes=False)
+def path_states_list():
+    """ Function display en templated state and amenities list
+    in 6-index.html (web_static)"""
+    all_states = storage.all(State).values()
+    all_amenities = storage.all(Amenity).values()
+
+    return render_template(
+        '10-hbnb_filters.html',
+        states=all_states,
+        amenities=all_amenities
+    )
 
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    """closes the storage on teardown"""
+def teardown_close_session(exception):
+    """ Function to be called when the application context ends. """
     storage.close()
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
